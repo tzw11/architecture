8557538eb15565ea67d3b69bb583e714839f2e75
wilsoncao <275239608@qq.com>
2016-02-10 01:33:08 -0500

    systemC

--- Sample1/PlumberC.java
+++ Sample1/PlumberC.java
@@ -1,3 +1,5 @@
+import java.util.logging.Filter;
+
 /******************************************************************************************************************
 * File:Plumber.java
 * Course: 17655
@@ -18,7 +20,7 @@
 * Internal Methods: None
 *
 ******************************************************************************************************************/
-public class PlumberB
+public class PlumberC
 {
    public static void main( String argv[])
    {
@@ -26,43 +28,40 @@ public class PlumberB
         * Here we instantiate three filters.
         ****************************************************************************/
 
-        SourceFilter Filter1 = new SourceFilter("FlightData.dat");
-        // MiddleFilter Filter2 = new MiddleFilter();
-         // TemperatureConvertor Filter2 = new TemperatureConvertor();
-//        TempAltPressFilter Filter2 = new TempAltPressFilter();
-//        TemperatureConvertor Filter3 = new TemperatureConvertor();
-//        AltitudeConvertor Filter4 = new AltitudeConvertor();
-        LessThan10k Filter5 = new LessThan10k();
+        SourceFilter Filter1 = new SourceFilter("SubSetA.dat");
+        SourceFilter Filter2 = new SourceFilter("SubSetB.dat");
+        MergeFilter Filter3 = new MergeFilter();
+        LessThan10k Filter4 = new LessThan10k();
+        WildPointFilter Filter5 = new WildPointFilter(6);
         SinkFilter Filter6 = new SinkFilter("wildPoint.dat");
-        // AltitudeConvertor Filter2 = new AltitudeConvertor();
-
-        SinkFilter Filter7 = new SinkFilter("outputB.dat");
-        //SinkTest Filter7 = new SinkTest();
+        SinkFilter Filter7 = new SinkFilter("LessThan10K.dat");
+        SinkFilter Filter8 = new SinkFilter("outputC");
 
         /****************************************************************************
         * Here we connect the filters starting with the sink filter (Filter 1) which
         * we connect to Filter2 the middle filter. Then we connect Filter2 to the
         * source filter (Filter3).
         ****************************************************************************/
-        Filter7.Connect(Filter5, 0, 0);
+        Filter3.Connect(Filter1, 0, 0);
+        Filter3.Connect(Filter2, 0, 1);
+        Filter4.Connect(Filter3, 0, 0);
+        Filter5.Connect(Filter4, 0, 0);
+        Filter7.Connect(Filter4, 1, 0);
         Filter6.Connect(Filter5, 1, 0);
-//        Filter5.Connect(Filter4, 0, 0);
-        Filter5.Connect(Filter1,0,0);
-//        Filter4.Connect(Filter3, 0, 0);
-//        Filter3.Connect(Filter2, 0, 0); // This esstially says, "connect Filter3 input port to Filter2 output port
-//        Filter2.Connect(Filter1, 0, 0); // This esstially says, "connect Filter2 intput port to Filter1 output port
+        Filter8.Connect(Filter5, 0, 0);
 
         /****************************************************************************
         * Here we start the filters up. All-in-all,... its really kind of boring.
         ****************************************************************************/
 
         Filter1.start();
-//        Filter2.start();
-//        Filter3.start();
-//        Filter4.start();
+        Filter2.start();
+        Filter3.start();
+        Filter4.start();
         Filter5.start();
         Filter6.start();
         Filter7.start();
+        Filter8.start();
 
 
    } // main
